8c551087b8178366970d8231e56e57638dd038d1
Wilmer Hdez <ingenierowily@gmail.com>
2022-02-25 16:22:51 -0500

    Mi programa en Py

---
+++ Adivinador.py
@@ -0,0 +1,21 @@
+from random import randint as azar
+ganador = azar(1,11)
+
+contador = 0
+num = int(input('Adivina que numero es el ganador del 1 al 10:  '))
+i = 0
+while i == 0:
+     if num > ganador:
+        print('El numero indicado es muy alto')
+        contador = contador + 1
+        num = int(input('Intentalo nuvamente: '))
+     elif num < ganador:
+        print('El numero indicado es muy bajo')
+        contador = contador + 1
+        num = int(input('Intentalo nuvamente: '))
+     else:
+        print('Has acertado al numero ganador')
+        contador = contador + 1
+        i = 1
+    
+print(f'Felicitaciones, lo adivinaste en {contador} intentos')
